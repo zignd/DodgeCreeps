c4334eff630ad5879abebc2ea8898f77ea3fab56
Zignd <zignd.igor@gmail.com>
2021-01-15 23:50:19 -0300

    Fix animations based directions

--- Player.gd
+++ Player.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	hide()
 
 func _process(delta):
 	var velocity = Vector2()
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -18,6 +21,7 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -28,3 +32,20 @@ func _process(delta):
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
