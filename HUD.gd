3a5e64d98c710f5732fc34b4e86dde6388772a86
Zignd <zignd.igor@gmail.com>
2021-01-17 15:18:25 -0300

    Finish HUD and add it to Main

--- HUD.gd
+++ HUD.gd
@@ -17,3 +17,13 @@ func show_game_over():
 	# Make a one-shot timer and wait for it to finish.
 	yield(get_tree().create_timer(1), "timeout")
 	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
+
+func _on_MessageTimer_timeout():
+	$Message.hide()
