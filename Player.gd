0a2a6d30139c531717867ad29b17f72bf71f511a
Zignd <zignd.igor@gmail.com>
2021-01-24 15:36:18 -0300

    Add configuration to export

--- Player.gd
+++ Player.gd
@@ -4,23 +4,39 @@ signal hit
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var target = Vector2() # Holds the clicked position.
 
 
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 func _process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	# Move towards the target and stops when close.
+	if position.distance_to(target) > 10:
+		velocity = target - position
+	
+# Remove keyboard controls.
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -29,6 +45,10 @@ func _process(delta):
 		$AnimatedSprite.stop()
 	
 	position += velocity * delta
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
@@ -44,8 +64,3 @@ func _on_Player_body_entered(body):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
